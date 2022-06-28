886ee5b121106e20511df3e54d3ced5e0c5ff955
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-06-28 15:37:53 -0400

    Change one of the hourly-rate to be more than 40

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -13,6 +13,6 @@ puts "----------"
 @store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 60)
 @store1.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 50)
 @store1.employees.create(first_name: "Jack", last_name: "Daniels", hourly_rate: 70)
-@store2.employees.create(first_name: "Chris", last_name: "Virani", hourly_rate: 32)
+@store2.employees.create(first_name: "Chris", last_name: "Virani", hourly_rate: 45)
 @store2.employees.create(first_name: "Khurram", last_name: "Peg", hourly_rate: 78)
 @store2.employees.create(first_name: "Robert", last_name: "Resha", hourly_rate: 82)
